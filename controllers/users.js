07639a27e9e48d0929a287149c91c2f6b11478ba
W1nde <markshadpalov@gmail.com>
2022-05-27 13:44:16 +0700

    jwt key

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,13 @@
 require('dotenv').config();
-const bcrypt = require
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const CastError = require('../errors/CastError');
+const Conflict = require('../errors/Conflict');
+const NotFound = require('../errors/NofFound');
+const ValidationError = require('../errors/ValidationError');
+
+const { JWT_SECRET = '2B4B6150645367566B5970337336763979244226452948404D6351655468576D' } = process.env;
+
