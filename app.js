87deba383a6fc954349cfdbc4ac7eab26b6f35f7
W1nde <markshadpalov@gmail.com>
2022-05-29 15:38:57 +0700

    CORS expansion

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ const allowedCors = [
   'http://localhost:3000',
   'https://localhost:3000',
   'localhost:3000',
+  'http://movies-explorer.nomoreparties.sbs', // адрес бэкенда
+  'https://movies-explorer.nomoreparties.sbs',
+  'http://movies-explorer-site.nomoreparties.sbs', // адрес клиентской части
+  'https://movies-explorer-site.nomoreparties.sbs',
 ];
 
 // Обработка CORS-запросов
