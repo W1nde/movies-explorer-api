eab9562a57a42ebe93a2d6567faffd5881764b56
W1nde <markshadpalov@gmail.com>
2022-05-29 20:45:00 +0700

    app fixes

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use ((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const reqHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHOD = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+  const DEFAULT_ALLOWED_METHOD = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Origin', origin);
   res.header('Acess-Controll-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
