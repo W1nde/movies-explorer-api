d1e67684c2e8d2e7e9b600c612e0074f77b3ee29
W1nde <markshadpalov@gmail.com>
2022-06-03 14:48:38 +0700

    find user by credentials add

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const mongoose = require('mongoose');
 const { default: isEmail } = require('validator/lib/isEmail');
+
+const Unauthorized = require('../errors/Unauthorized');
 
 // модель пользователя
 
@@ -27,6 +30,27 @@ const userSchema = new mongoose.Schema ({
     minlength: 2,
     maxlength: 30,
   },
-})
+});
+
+// поиска пользователя
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }, { runValidators: true })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(
+          new Unauthorized(errMessages.loginError),
+        );
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new Unauthorized(errMessages.loginError),
+          );
+       }
+      return user;
+    });
+  });
+}
 
 module.exports = mongoose.model('user', userSchema);
