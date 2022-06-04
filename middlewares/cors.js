8c841e3a77fcaf87494c3495562ea9dd5e4cf36f
W1nde <markshadpalov@gmail.com>
2022-06-04 16:33:26 +0700

    cors rebuild

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,18 @@
 const { allowedCors } = require('../utils/allowedCors');
 
 // обработка CORS-запросов
-app.use ((req, res, next) => {
+module.exports = (function (req, res, next) {
   const { origin } = req.headers;
-  const { method } = req;
-  const reqHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHOD = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Acess-Controll-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHOD)
-    res.header('Access-Control-Allow-Headers', reqHeaders);
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   return next();
