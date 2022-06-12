dafd60daca6de0621908700d0ea3f5bf2547ea7b
W1nde <markshadpalov@gmail.com>
2022-06-13 01:21:44 +0700

    movie model rebuild

--- models/movie.js
+++ models/movie.js
@@ -1,79 +1,52 @@
 const mongoose = require('mongoose');
-const { default: isURL } = require('validator/lib/isURL');
-
-// модель фильма
 
 const movieSchema = new mongoose.Schema({
   country: {
     type: String,
     required: true,
   },
-
   director: {
     type: String,
     required: true,
   },
-
   duration: {
     type: Number,
     required: true,
   },
-
   year: {
-    type: Number,
+    type: String,
     required: true,
-    length: 4,
   },
-
   description: {
     type: String,
     required: true,
   },
-
   image: {
     type: String,
     required: true,
-    validate: {
-      validator: (v) => isURL(v),
-      message: 'Некорректная ссылка на постер',
-    },
   },
-
   trailerLink: {
     type: String,
     required: true,
-    validate: {
-      validator: (v) => isURL(v),
-      message: 'Некорректная ссылка на трейлер',
-    },
   },
-
   thumbnail: {
     type: String,
     required: true,
-    validate: {
-      validator: (v) => isURL(v),
-      message: 'Некорректная ссылка на миниатюру',
-    },
   },
-
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'user',
   },
-
-  moviedId: {
-    type: mongoose.Schema.Types.ObjectId,
+  movieId: {
+    type: Number,
     required: true,
-    // ref ???
+    ref: 'movie',
   },
-
   nameRU: {
     type: String,
     required: true,
   },
-
   nameEN: {
     type: String,
     required: true,
